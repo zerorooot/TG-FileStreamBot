e7d128876affd5f0450025e9d2b505c987a24ee4
zerorooot <msj1013654754@gmail.com>
2024-07-12 15:40:07 +0800

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -25,18 +25,21 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
     ),
     group=4,
 )
+
 async def media_receive_handler(_, m: Message):
     if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS)):
-        return await m.reply("You are not <b>allowed to use</b> this <a href='https://github.com/EverythingSuckz/TG-FileStreamBot'>bot</a>.", quote=True)
+        return await m.reply("You are not <b>allowed to use</b> this <a href=' '>bot</a >.", quote=True)
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     file_hash = get_hash(log_msg, Var.HASH_LENGTH)
-    stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={file_hash}&name={quote_plus(get_name(m))}&caption={quote_plus(m.caption)}"
+    name = get_name(m)
+    caption = m.caption if m.caption is not None else name
+    stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(name)}?hash={file_hash}&name={quote_plus(name)}&caption={quote_plus(caption)}"
     short_link = f"{Var.URL}{file_hash}{log_msg.id}"
     logger.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
     try:
         await m.reply_text(
-            text="<code>{}</code>\n(<a href='{}'>shortened</a>)".format(
-                stream_link, short_link
+            text="<code>{}</code>".format(
+                stream_link
             ),
             quote=True,
             parse_mode=ParseMode.HTML,
@@ -46,8 +49,8 @@ async def media_receive_handler(_, m: Message):
         )
     except errors.ButtonUrlInvalid:
         await m.reply_text(
-            text="<code>{}</code>\n\nshortened: {})".format(
-                stream_link, short_link
+            text="<code>{}</code>".format(
+                stream_link
             ),
             quote=True,
             parse_mode=ParseMode.HTML,
